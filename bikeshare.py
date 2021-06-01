924ec4907282a8738237110717ec8366f7f200fa
Maika <maika.stehr@gmail.com>
2021-06-01 18:04:31 +0200

    link library for numpy and pandas added

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,6 @@
 import time
-import pandas as pd
-import numpy as np
+import pandas as pd # get to pd library: https://pandas.pydata.org/
+import numpy as np # get to np library https://numpy.org/
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
